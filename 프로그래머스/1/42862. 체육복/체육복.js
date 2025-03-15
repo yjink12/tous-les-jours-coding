65e57a35492b6fa3c318f298cabb81d802076e17
YuJin <82390580+yjink12@users.noreply.github.com>
2025-03-15 13:20:12 +0900

    [level 1] Title: 체육복, Time: 0.34 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,24 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    
+    const actualLost = lost.filter(student => !reserve.includes(student));
+    const actualReserve = reserve.filter(student => !lost.includes(student));
+    
+    let sortLost = actualLost.sort((a, b) => a - b);
+    let sortReserve = actualReserve.sort((a, b) => a - b);
+        
+    for (let student of sortLost) {
+        const canShare = sortReserve.filter((re) => re == student-1 || re == student+1);
+        
+        if (2 == canShare.length) {
+            sortLost = sortLost.filter((lt) => lt !== student);
+            sortReserve = sortReserve.filter((re) => re !== canShare.sort((a, b) => a - b)[0]);
+        } else if (1 == canShare.length) {
+            sortLost = sortLost.filter((lt) => lt !== student);
+            sortReserve = sortReserve.filter((re) => re !== canShare[0]);
+        }
+    }
+    
+    answer = n - sortLost.length;
+    return answer;
+}
