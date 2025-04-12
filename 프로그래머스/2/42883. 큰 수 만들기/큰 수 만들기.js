2ce6af8cd0defc153a17ac9b4d61b2b95c2d3958
YuJin <82390580+yjink12@users.noreply.github.com>
2025-04-12 14:42:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 58.11 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -3,7 +3,7 @@ function solution(number, k) {
     const stack = [];
     
     for (const num of number) {
-        while(k > 0 && stack[stack.length -1] < num) {
+        while(k > 0 && stack.length > 0 && stack[stack.length -1] < num) {
             stack.pop();
             k--;
         }
